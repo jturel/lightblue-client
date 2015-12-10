a4c3087137165379aa7aa4fee52691cd46c46043
Davis Wahl <dwahl@redhat.com>
2015-12-10 00:30:25 -0500

    Fix require

--- lib/lightblue.rb
+++ lib/lightblue.rb
@@ -7,7 +7,7 @@ require 'lightblue/projection'
 require 'lightblue/find_manager'
 require 'lightblue/version'
 require 'lightblue/metadata'
-require 'lightblue-client/railtie' if defined?(Rails)
+require 'lightblue/railtie' if defined?(Rails)
 
 module Lightblue
   def self.resources
