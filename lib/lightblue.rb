1834d49ff411f52c26be754e8523e5d2a6cea812
Davis Wahl <dwahl@redhat.com>
2015-12-10 00:28:19 -0500

    Add railtie

--- lib/lightblue.rb
+++ lib/lightblue.rb
@@ -4,6 +4,10 @@ require 'lightblue/entity'
 require 'lightblue/visitors'
 require 'lightblue/version'
 require 'lightblue/metadata'
+require 'lightblue-client/railtie' if defined?(Rails)
 
 module Lightblue
+  def self.resources
+    Pathname.new(File.expand_path '../resources', File.dirname(__FILE__))
+  end
 end
